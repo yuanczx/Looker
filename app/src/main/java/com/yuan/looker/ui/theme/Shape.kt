178a9ed2aeb22a16c0483bf0629230f086f38841
yuanczx <yuanczx@foxmail.com>
2021-08-14 15:44:18 +0800

    init repo

---
+++ app/src/main/java/com/yuan/looker/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.yuan.looker.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
